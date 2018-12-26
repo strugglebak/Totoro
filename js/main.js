cfefc688902953ae43c7edf140537537e42cdc22
xm <dxp157@sohu.com>
2018-12-26 16:43:24 +0800

    add comments in main.js

--- js/main.js
+++ js/main.js
@@ -1,149 +1,138 @@
 !function() {
   let cssCode = `
+  /* 首先需要画个背景 */
   .preview {
     background: #E1E7d3;
   }
 
+  /* 然后是鼻子 */
   .nose {
-    position: absolute;
-    top: 106px;
-    left: 50%;
-
     width: 30px;
     height: 10px;
     background: #5F5852;
     border-radius: 30px/10px;
-
+    position: absolute;
+    top: 106px;
+    left: 50%;
     margin-left: -15px;
     z-index: 6;
   }
 
+  /* 再画个眼镜吧 */
   .eye {
-    position: absolute;
-    top: 95px;
-
     width: 24px;
     height: 24px;
     background: #fff;
     border-radius: 50%;
+    position: absolute;
+    top: 95px;
     z-index: 6;
   }
   .eye::after {
-    position: absolute;
-    top: 7px;
-    left: 7px;
-
     content: '';
     display: block;
     width: 9px;
     height: 9px;
     background: #5A534E;
     border-radius: 50%;
+    position: absolute;
+    top: 7px;
+    left: 7px;
   }
+  /* 看，往左! */
   .eye.left {
     left: 74px;
   }
+  /* 看，往右! */
   .eye.right {
     right: 74px;
   }
 
+  /* 然后画这个苯苯的龙猫的头 */
   .head {
-    position: absolute;
-    top: 59px;
-    left: 50%;
-
     width: 126px;
     height: 75px;
     background: #A4978E;
     border-radius: 100px/60px;
-
+    position: absolute;
+    top: 59px;
+    left: 50%;
     margin-left: -63px;
     z-index: 1;
   }
   .bodyFiller {
-    position: absolute;
-    top: 107px;
-    left: 50%;
-
     width: 130px;
     height: 56px;
     background: #A4978E;
-
+    position: absolute;
+    top: 107px;
+    left: 50%;
     margin-left: -65px;
     z-index: 2;
   }
+  /* 圆圆的肚皮... */
   .belly {
-    position: absolute;
-    top: 157px;
-    left: 50%;
-
     width: 200px;
     height: 214px;
     background: #EFDCCB;
     border-radius: 100px/107px;
-
+    position: absolute;
+    top: 157px;
+    left: 50%;
     margin-left: -100px;
     z-index: 6;
   }
   .bellyFiller {
-    position: absolute;
-    left: 50%;
-    top: 163px;
-
     width: 194px;
     height: 63px;
     background: #A4978E;
-
+    position: absolute;
+    left: 50%;
+    top: 163px;
     margin-left: -97px;
     z-index: 2;
   }
+  /*恩... 下肢唉，有点难画呢... */
   .lowerLimb {
-    position: absolute;
-    top: 172px;
-    left: 50%;
-
     width: 220px;
     height: 225px;
     background: #a4978e;
     border-radius: 110px;
-
+    position: absolute;
+    top: 172px;
+    left: 50%;
     margin-left: -110px;
     z-index: 1;
   }
   .prune > .pruneLowerLimb {
-    position: absolute;
-    top: 378px;
-    left: 50%;
-
     width: 8px;
     height: 18px;
     background: #E1E7D3;
-
+    position: absolute;
+    top: 378px;
+    left: 50%;
     margin-left: -4px;
     z-index: 999;
     transform: rotate(45deg);
   }
   .prune > .pruneFeet {
-    position: absolute;
-    top: 381px;
-    left: 50%;
-
     width: 124px;
     height: 20px;
     background: #E1E7D3;
-
+    position: absolute;
+    top: 381px;
+    left: 50%;
     margin-left: -62px;
     z-index: 999;
   }
 
+  /*啊，终于到上肢了,耶!!!*/
   .arm {
-    position: absolute;
-    top: 83px;
-
     width: 35px;
     height: 131px;
     background: #A4978E;
-
+    position: absolute;
+    top: 83px;
     z-index: 4;
   }
   .arm.left {
@@ -155,15 +144,14 @@
     transform: rotate(-27deg);
   }
 
+  /* 好了现在开始画个手手手~~~ */
   .hand {
-    position: absolute;
-    top: 192px;
-
     width: 38px;
     height: 104px;
     background: #A4978E;
     border-radius: 40px/100px;
-
+    position: absolute;
+    top: 192px;
     z-index: 3;
   }
   .hand.left {
@@ -173,30 +161,30 @@
     right: 0;
   }
 
+  /* 然后是这只傻呼呼龙猫的花斑，额
+   * 这个应该是它的花斑吧...
+   * */
   .piebald {
-    position: absolute;
-    top: 180px;
-    left: 50%;
-    margin-left: -15px;
-
     width: 30px;
     height: 14px;
     background: #89775A;
     border-radius: 100px/50px;
-
+    position: absolute;
+    top: 180px;
+    left: 50%;
+    margin-left: -15px;
     z-index: 7;
   }
   .piebald::after {
-    position: absolute;
-    top: 4px;
-    left: -7px;
-
     content: '';
     display: block;
     width: 44px;
     height: 30px;
     background: #EFDCCB;
     border-radius: 100px/80px;
+    position: absolute;
+    top: 4px;
+    left: -7px;
   }
   .piebald.spots1 {
     top: 180px;
@@ -232,13 +220,16 @@
     transform: rotate(-345deg);
   }
 
+  /* 好多花斑终于!! 画完了!
+   * 现在开始画耳朵!!!
+   * 哦错了是耳根 = = !!
+   * */
   .earRoot {
-    position: absolute;
-    top: 55px;
-
     width: 9px;
     height: 25px;
     background: #A4978E;
+    position: absolute;
+    top: 55px;
   }
   .earRoot.left {
     left: 80px;
@@ -249,71 +240,66 @@
     transform: rotate(-348deg);
   }
 
+  /* 好了现在是正儿八经的耳朵时间....*/
   .ear-left {
-    position: absolute;
-    top: -5px;
-    left: 60px;
-
     border: 15px solid transparent;
     border-bottom: 41px solid #A4978E;
     transform: rotate(350deg);
+    position: absolute;
+    top: -5px;
+    left: 60px;
   }
   .ear-left::after {
+    content: '';
+    display: block;
+    border: 15px solid transparent;
+    border-top: 10px solid #A4978E;
     position: absolute;
     top: 39px;
     left: -15px;
-
+  }
+  .ear-right {
+    border: 15px solid transparent;
+    border-bottom: 41px solid #A4978E;
+    transform: rotate(-350deg);
+    position: absolute;
+    top: -5px;
+    right: 60px;
+  }
+  .ear-right::after {
     content: '';
     display: block;
     border: 15px solid transparent;
     border-top: 10px solid #A4978E;
-  }
-
-  .ear-right {
-      position: absolute;
-      top: -5px;
-      right: 60px;
-
-      border: 15px solid transparent;
-      border-bottom: 41px solid #A4978E;
-      transform: rotate(-350deg);
-  }
-  .ear-right::after {
     position: absolute;
     top: 39px;
     right: -15px;
-
-    content: '';
-    display: block;
-    border: 15px solid transparent;
-    border-top: 10px solid #A4978E;
   }
 
+  /* 接下来你猜是啥？
+   * 当然是重头戏(雾..)
+   * 其实就是 totoro 啦
+   * */
   .totoro {
-    position: absolute;
-    top: 400px;
-
     width: 255px;
     height: 80px;
+    position: absolute;
+    top: 400px;
     }
     .to1 {
+        width: 18px;
+        height: 66px;
+        background: #A4978E;
+        border-top-left-radius: 50% 60%;
+        border-top-right-radius: 50% 60%;
+        border-bottom-right-radius: 50% 40%;
+        border-bottom-left-radius: 50% 40%;
         position: absolute;
         top: 0px;
         left: 70px;
-
-        width: 18px;
-        height: 66px;
-        background: #A4978E;
-        border-top-left-radius: 50% 60%;
-        border-top-right-radius: 50% 60%;
-        border-bottom-right-radius: 50% 40%;
-        border-bottom-left-radius: 50% 40%;
         transform: rotate(5deg);
       }
     .to1::after {
-        position: absolute;
-        top: 20px;
-
         content: '';
         display: block;
         width: 33px;
@@ -323,12 +309,10 @@
         border-top-right-radius: 50% 60%;
         border-bottom-right-radius: 50% 40%;
         border-bottom-left-radius: 50% 40%;
+        position: absolute;
+        top: 20px;
       }
     .to2 {
-        position: absolute;
-        top: 0;
-        left: 110px;
-
         width: 18px;
         height: 66px;
         background: #A4978E;
@@ -337,11 +321,11 @@
         border-bottom-right-radius: 50% 40%;
         border-bottom-left-radius: 50% 40%;
         transform: rotate(5deg);
+        position: absolute;
+        top: 0;
+        left: 110px;
       }
     .to2::after {
-        position: absolute;
-        top: 20px;
-
         content: '';
         display: block;
         width: 33px;
@@ -351,30 +335,34 @@
         border-top-right-radius: 50% 60%;
         border-bottom-right-radius: 50% 40%;
         border-bottom-left-radius: 50% 40%;
+        position: absolute;
+        top: 20px;
       }
     .ro {
-        position: absolute;
-        top: 21px;
-        left: 150px;
-
         width: 43px;
         height: 43px;
         border-radius: 12px;
         background: #A4978E;
+        position: absolute;
+        top: 21px;
+        left: 150px;
         transform: rotate(5deg);
       }
     .ro::after {
-        position: absolute;
-        top: 13px;
-        left: 11px;
-
         content: '';
         display: block;
         width: 22px;
         height: 17px;
         background: #E1E7d3;
         border-radius: 5px;
+        position: absolute;
+        top: 13px;
+        left: 11px;
       }
+
+    /* 终于画完啦，这只 totoro 龙猫就送给你吧
+     * ^.^
+     * */
   `;
 
   let content = document.querySelector('#cssCode');
